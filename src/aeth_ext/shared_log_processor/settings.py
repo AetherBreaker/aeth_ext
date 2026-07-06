a3777b478bc2f37f0cc207f02c0fb9c2a5f26040
Jacob Ogden <jacob.ogden@yahoo.com>
2026-07-06 14:17:13 -0400

    a

--- src/aeth_ext/shared_log_processor/settings.py
+++ src/aeth_ext/shared_log_processor/settings.py
@@ -23,8 +23,6 @@ class Settings(BaseSettings):
 
   debug_wait_for_client: bool = False
 
-  file_serve_public_url: Annotated[str | None, Field(alias="FILE_SERVE_PUBLIC_URL")] = (
-    "https://r68g367p9a5jnsic9kwx8dti.108.61.75.238.sslip.io"
-  )
+  file_serve_public_url: Annotated[str | None, Field(alias="FILE_SERVE_PUBLIC_URL")] = "https://som.sweetfiretobacco.com"
   file_serve_host: Annotated[str, Field(alias="FILE_SERVE_HOST")] = "localhost"
   file_serve_port: Annotated[int, Field(alias="FILE_SERVE_PORT")] = 8080
