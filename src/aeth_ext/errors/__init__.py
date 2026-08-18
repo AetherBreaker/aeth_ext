3e987d588630d2f96842c14f00a12f27bbedfecc
Jacob Ogden <jacob.ogden@yahoo.com>
2026-08-18 13:38:26 -0400

    Trimmed the errors exports

--- src/aeth_ext/errors/__init__.py
+++ src/aeth_ext/errors/__init__.py
@@ -7,15 +7,10 @@ from .err_handling import (
   report_exc,
   trigger_shutdown,
 )
-from .exception_trail import ExceptionTrail, OriginCategory, TrailEntry, build_exception_trail
 
 __all__ = [
-  "ExceptionTrail",
-  "OriginCategory",
-  "TrailEntry",
   "alert",
   "alert_exception",
-  "build_exception_trail",
   "handle_fatal_exc_async",
   "handle_fatal_exc_sync",
   "report_exc",
