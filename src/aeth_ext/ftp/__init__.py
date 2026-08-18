d34b8d9ae7a087e992aac7ce076454d62433f2f4
Jacob Ogden <jacob.ogden@yahoo.com>
2026-08-18 12:55:30 -0400

    Trimmed down the ftp package exports to only what should be exported by the top level of the subpackage. Other things should be imported by the more explicit imports (as that imparts information about what is being imported, such as types)

--- src/aeth_ext/ftp/__init__.py
+++ src/aeth_ext/ftp/__init__.py
@@ -7,22 +7,10 @@ directly, if you want the concrete type without the `isinstance` dispatch). Ever
 
 # Local folder imports
 from .credentials import FTPCredentials, SFTPCredentials
-from .errors import ServerNotAvailableError
 from .factory import create_ftp_adapter
-from .pool.ftp_adapter import FTPAdapter
-from .pool.sftp_adapter import SFTPAdapter
-from .session import AdaptedFTP, AdaptedSFTP
-from .types import HandleProvider, ListDirResult
 
 __all__ = [
-  "AdaptedFTP",
-  "AdaptedSFTP",
-  "FTPAdapter",
   "FTPCredentials",
-  "HandleProvider",
-  "ListDirResult",
-  "SFTPAdapter",
   "SFTPCredentials",
-  "ServerNotAvailableError",
   "create_ftp_adapter",
 ]
