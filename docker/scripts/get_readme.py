66d8065212102c63d942083e6d7024a887dc5666
Jacob Ogden <jacob.ogden@yahoo.com>
2026-09-06 23:30:42 -0400

    a

--- docker/scripts/get_readme.py
+++
@@ -1,9 +0,0 @@
-"""Print the project readme path from pyproject, without a trailing newline."""
-
-# Standard library imports
-import tomllib
-
-with open("/app/pyproject.toml", "rb") as f:
-  data = tomllib.load(f)
-
-print(data.get("project", {}).get("readme", ""), end="")
